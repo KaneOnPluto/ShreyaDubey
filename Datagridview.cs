f5fc1bb8df47f19c6e5a3cad8519f4bf0b35b029
KaneOnPluto <kushalpatel8989@gmail.com>
2026-04-24 00:55:06 +0530

    Create Datagridview.cs

---
+++ Datagridview.cs
@@ -0,0 +1,6 @@
+dataGridView1.ColumnCount = 2;
+dataGridView1.Columns[0].Name = "ID";
+dataGridView1.Columns[1].Name = "Name";
+
+dataGridView1.Rows.Add("1", "Kane");
+dataGridView1.Rows.Add("2", "RKU");
